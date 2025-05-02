11641a0db3a72243bcca6f248ce9f74b6c367805
Gab <Gaby@Gabs-MacBook-Pro.local>
2025-05-02 20:40:42 +0300

    pushing

--- api/server.js
+++ api/server.js
@@ -21,7 +21,7 @@ app.use("/api/v1/users", userRoutes);
 
 // Admin endpoints
 app.use("/api/v1/admin", adminRoutes);
-
+// HELLOOOOOOOO
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: "Something went wrong!" });
