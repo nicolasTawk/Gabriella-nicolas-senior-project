3b098cb3bf1bdfd757a66daee90d6425906993ef
nicolasTawk <nicolas.tawk@std.balamand.edu.lb>
2025-06-11 11:41:23 +0300

    testing1

--- api/database/db_connection.js
+++ api/database/db_connection.js
@@ -11,7 +11,7 @@ const { Sequelize } = require("sequelize");
 //    }
 //  }
 
-const sequelize = new Sequelize("mysql://root:HBaqINXLCIEQyoNxxKUZomdbnJpzUixY@ballast.proxy.rlwy.net:58230/railway",
+const sequelize = new Sequelize(process.env.DB_URL,
   // process.env.DB_NAME,
   // process.env.DB_USER,
   // process.env.DB_PASSWORD,
